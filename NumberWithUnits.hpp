a3b3ad7a8b2592e51e7838eca6179b582789edd7
marwanhresh <62513189+marwanhresh@users.noreply.github.com>
2021-04-26 15:04:48 +0300

    Update NumberWithUnits.hpp

--- NumberWithUnits.hpp
+++ NumberWithUnits.hpp
@@ -9,49 +9,55 @@ using namespace std;
 
 namespace ariel{ class NumberWithUnits; }
 ariel::NumberWithUnits operator*(double const & lhs, ariel::NumberWithUnits const & rhs);
-ostream& operator<<(ostream& o, ariel::NumberWithUnits n);
+ostream& operator<<(ostream& o, const ariel::NumberWithUnits & n);
 istream& operator>>(istream& i, ariel::NumberWithUnits& n);
 
 namespace ariel{
-	class NumberWithUnits{
-	private:
-		static unordered_map<string, list< pair<string, double> >> unitsMap;
-		string unit;
-		double amount;
+    class NumberWithUnits{
+    private:
+        static unordered_map<string, list< pair<string, double> >> unitsMap;
+        static const double EPSILON;
+        string unit;
+        double amount;
 
-		static bool isUnitExists(string unit);
-		bool isPossibleToConvert(NumberWithUnits const & other, double & convertedValue) const;
+        static bool isUnitExists(string const & unit);
+        bool isPossibleToConvert(NumberWithUnits const & other, double & convertedValue) const;
         bool isSameUnit(NumberWithUnits const & other) const;
-	public:
-		static void read_units(ifstream& units_file);
+        static double convertFromTo(const NumberWithUnits& a, const NumberWithUnits& b);
 
-		NumberWithUnits(double amount, string unit);
+        static bool isEqual(double a, double b);
+        static bool isBigger(double a, double b);
+        static bool isSmaller(double a, double b);
+    public:
+        static void read_units(ifstream& units_file);
 
-		NumberWithUnits operator+(NumberWithUnits const & rhs) const;
-		NumberWithUnits operator+() const;
-		NumberWithUnits & operator+=(NumberWithUnits const & rhs);
+        NumberWithUnits(double amount, string const & unit);
 
-		NumberWithUnits operator-(NumberWithUnits const & rhs) const;
-		NumberWithUnits operator-() const;
-		NumberWithUnits & operator-=(NumberWithUnits const & rhs);
+        NumberWithUnits operator+(NumberWithUnits const & rhs) const;
+        NumberWithUnits operator+() const;
+        NumberWithUnits & operator+=(NumberWithUnits const & rhs);
 
-		bool operator==(NumberWithUnits const & rhs) const;
-		bool operator!=(NumberWithUnits const & rhs) const;
-		bool operator<(NumberWithUnits const & rhs) const;
-		bool operator<=(NumberWithUnits const & rhs) const;
-		bool operator>(NumberWithUnits const & rhs) const;
-		bool operator>=(NumberWithUnits const & rhs) const;
+        NumberWithUnits operator-(NumberWithUnits const & rhs) const;
+        NumberWithUnits operator-() const;
+        NumberWithUnits & operator-=(NumberWithUnits const & rhs);
 
-		NumberWithUnits & operator++();
-		NumberWithUnits operator++(int);
+        bool operator==(NumberWithUnits const & rhs) const;
+        bool operator!=(NumberWithUnits const & rhs) const;
+        bool operator<(NumberWithUnits const & rhs) const;
+        bool operator<=(NumberWithUnits const & rhs) const;
+        bool operator>(NumberWithUnits const & rhs) const;
+        bool operator>=(NumberWithUnits const & rhs) const;
 
-		NumberWithUnits & operator--();
-		NumberWithUnits operator--(int);
+        NumberWithUnits & operator++();
+        NumberWithUnits operator++(int);
 
-		NumberWithUnits operator*(double const & rhs) const;
+        NumberWithUnits & operator--();
+        NumberWithUnits operator--(int);
 
-		friend NumberWithUnits (::operator*(double const & lhs, ariel::NumberWithUnits const & rhs));
-		friend ostream& ::operator<<(ostream& o, ariel::NumberWithUnits n);
-		friend istream& ::operator>>(istream& i, ariel::NumberWithUnits& n);
-	};
+        NumberWithUnits operator*(double const & rhs) const;
+
+        friend NumberWithUnits(::operator*(double const & lhs, ariel::NumberWithUnits const & rhs));
+        friend ostream& ::operator<<(ostream& o, const ariel::NumberWithUnits & n);
+        friend istream& ::operator>>(istream& i, ariel::NumberWithUnits& n);
+    };
 }
